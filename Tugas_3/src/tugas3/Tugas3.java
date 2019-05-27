0a65477999ed6e48920037840cdce1849b46f370
RIfki-Muhammadi <50610473+RIfki-Muhammadi@users.noreply.github.com>
2019-05-27 16:32:15 +0800

    Add files via upload

---
+++ Tugas_3/src/tugas3/Tugas3.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package tugas3;
+
+/**
+ *
+ * @author Rifki
+ */
+public class Tugas3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+{
+        tokobuah saya = new tokobuah();
+        saya.menanam();
+        saya.jual();
+        saya.pesan();
+        
+        System.out.println("\nJumlah Pepaya   : "+saya.pepaya);
+        System.out.println("Jumlah Jeruk : "+saya.jeruk);
+        System.out.println("Jumlah Nanas   : "+saya.nanas);
+    }
+}}
+
---
+++ Tugas_3/src/tugas3/tokobuah.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package tugas3;
+
+/**
+ *
+ * @author Rifki
+ */
+public class tokobuah {
+    int pepaya   = 25;
+    int jeruk    = 25;
+    int nanas    = 25;
+    
+    void menanam(){
+        System.out.println("menanam pepaya, jeruk dan nanas");
+    }
+    void jual(){
+        System.out.println("Jual pepaya, jeruk dan nanas");
+    }
+    void pesan(){
+        System.out.println("Pemesanan pepaya, jeruk dan nanas");
+    }
+    
+
+  
+}
+
